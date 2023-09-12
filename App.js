c080ba6fece05f0c9229bf1cf9c7b41ea2f7b7c7
Clayton <persinger.clayton@gmail.com>
2023-09-12 14:21:28 -0700

    Create first component to display weather info

--- App.js
+++ App.js
@@ -1,29 +1,67 @@
 import { Text, View, SafeAreaView, StyleSheet } from 'react-native';
+import { Feather } from '@expo/vector-icons'; 
 
 const App = () => {
   return (
     <SafeAreaView style={styles.droidSafeArea}>
       <View style={styles.container}>
-        <Text>
-          Current Weather
-        </Text>
+      <Feather name="sun" size={100} color="black" />
+        <Text style={styles.temp}>6</Text>
+        <Text style={styles.feels}>Feels Like 5</Text>
+        <View style={styles.highLowWrapper}>
+          <Text style={styles.highLow}>High: 8</Text>
+          <Text style={styles.highLow}>Low: 6</Text>
+        </View>
       </View>
+      <View style={styles.bodyWrapper}>
+        <Text style={styles.description}>It's sunny</Text>
+        <Text style={styles.message}>It's perfect t-shirt weather</Text>
+      </View>
+
     </SafeAreaView>
   )
 };
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'pink',
     flex: 1,
+    justifyContent: 'center',
     alignItems: 'center',
   },
   wrapper: {
     flex: 1,
   },
   droidSafeArea: {
+    backgroundColor: 'pink',
     flex: 1,
     paddingTop: Platform.OS === 'android' ? 25 : 0
   },
+  temp: {
+    color: 'black',
+    fontSize: 48,
+  },
+  feels: {
+    color: 'black',
+    fontSize: 30
+  },
+  highLow: {
+    color: 'black',
+    fontSize: 20,
+  },
+  highLowWrapper: {
+    flexDirection: 'row',
+  },
+  bodyWrapper: {
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start',
+    paddingLeft: 25,
+    marginBottom: 40,
+  },
+  description: {
+    fontSize: 48
+  },
+  message: {
+    fontSize: 30
+  }
 })
 export default App;
