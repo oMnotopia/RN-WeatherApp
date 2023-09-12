6b1d57bcdfe45c65a3167de977f8abc26faf255d
Clayton <persinger.clayton@gmail.com>
2023-09-12 13:27:42 -0700

    First Commit

--- App.js
+++ App.js
@@ -3,9 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{height: 100, width: 100, backgroundColor: 'orange'}}>
+
     </View>
   );
 }
