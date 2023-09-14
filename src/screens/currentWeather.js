2f4b4463611cebe5272025fbd89119626fba82b1
Clayton <persinger.clayton@gmail.com>
2023-09-14 09:33:06 -0700

    Refactor code to make it look cleaner

--- src/screens/currentWeather.js
+++ src/screens/currentWeather.js
@@ -3,26 +3,39 @@ import { Feather } from '@expo/vector-icons';
 import RowText from '../components/RowText';
 
 const CurrentWeather = () => {
+
+  const {
+    container, 
+    droidSafeArea, 
+    temp, 
+    feels, 
+    highLowWrapper, 
+    highLow, 
+    bodyWrapper, 
+    description, 
+    message
+  } = styles
+
   return (
-    <SafeAreaView style={styles.droidSafeArea}>
-      <View style={styles.container}>
+    <SafeAreaView style={droidSafeArea}>
+      <View style={container}>
         <Feather name="sun" size={100} color="black" />
-        <Text style={styles.temp}>6</Text>
-        <Text style={styles.feels}>Feels Like 5</Text>
+        <Text style={temp}>6</Text>
+        <Text style={feels}>Feels Like 5</Text>
         <RowText 
           messageOne={"High: 8"} 
           messageTwo={"Low: 6"} 
-          containerStyles={styles.highLowWrapper} 
-          messageOneStyles={styles.highLow} 
-          messageTwoStyles={styles.highLow}
+          containerStyles={highLowWrapper} 
+          messageOneStyles={highLow} 
+          messageTwoStyles={highLow}
         />
       </View>
       <RowText 
           messageOne={"It's Sunny"} 
           messageTwo={"It's perfect t'shirt weather"} 
-          containerStyles={styles.bodyWrapper} 
-          messageOneStyles={styles.description} 
-          messageTwoStyles={styles.message}
+          containerStyles={bodyWrapper} 
+          messageOneStyles={description} 
+          messageTwoStyles={message}
       />
     </SafeAreaView>
   )
