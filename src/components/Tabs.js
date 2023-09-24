8d00cf72e931da66cf31025d2b03e9193fd2e6be
Clayton <persinger.clayton@gmail.com>
2023-09-24 14:12:39 -0700

    return upcomingweather as a child

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -37,13 +37,14 @@ const Tabs = ({weather}) => {
       </Tab.Screen>
       <Tab.Screen 
         name={'Upcoming'} 
-        component={UpcomingWeather} 
         options={{
           tabBarIcon: ({ focused }) => ( 
             <Feather name={'clock'} size={25} color={focused ? 'tomato' : 'black'} /> 
           )
         }} 
-      />
+      >
+        {() => <UpcomingWeather weatherData={weather.list}/>}
+      </Tab.Screen>
       <Tab.Screen 
         name={'City'} 
         component={City} 
